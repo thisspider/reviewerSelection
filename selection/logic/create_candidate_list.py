fc1ba5d741228d62316832f293f2d91c688eee7a
shirley zhang <shirleyyz9402@gmail.com>
2023-08-30 12:10:26 +0200

    Similarity comparison

--- selection/logic/create_candidate_list.py
+++ selection/logic/create_candidate_list.py
@@ -1,7 +1,11 @@
 from pyalex import Works
 import pandas as pd
 
-# oa_id_list = ["W2741809807", "W1560783210", "W2029057325", "W2160597895"]
+<<<<<<< HEAD
+oa_id_list = ["W2741809807", "W1560783210", "W2029057325", "W2160597895"]
+=======
+#oa_id_list = ["W2741809807", "W1560783210", "W2029057325", "W2160597895"]
+>>>>>>> 0e17258 (accept changes)
 
 def extract_refs(oa_id_list):
     """
