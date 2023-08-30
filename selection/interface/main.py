ead9e0b397f42cecd2d1000977f82fdf4017f0f9
Nicolas Klin <nklin02@gmail.com>
2023-08-30 13:02:30 +0200

    Add 3 steps to main and add dockerfile and setup.py

---
+++ selection/interface/main.py
@@ -0,0 +1,24 @@
+from selection.logic.pdf import PDF
+from selection.logic.create_candidate_list import *
+from selection.logic.restricted_reviewer import *
+# from selection.logic... import *
+
+
+#1 Link pdf to openalex
+# Extraction and matching
+
+#2 Create candidate list
+
+#3 Select reviewers from candidate list
+
+
+def pdf_matching(path: string):
+    pdf = PDF(path)
+
+    return pdf
+
+def candidate_list():
+    pass
+
+def select_reviewers():
+    pass
