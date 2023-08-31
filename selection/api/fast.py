bcd9cf80a98c1eca55cd9ddf1b3dbac3178fd47d
Nicolas Klin <nklin02@gmail.com>
2023-08-31 12:14:27 +0200

    Add sth to requirements.in, done with frontend basics and fastapi works.

--- selection/api/fast.py
+++ selection/api/fast.py
@@ -1,22 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from selection.interface.main import pdf_matching, candidate_df, select_reviewers
+from tempfile import NamedTemporaryFile
 
 app = FastAPI()
 
-@app.get("/select")
-def select(pdf_path: str):
+@app.post("/select")
+def select(uploaded_pdf: UploadFile):
     '''
     Give x recommendations for reviewers for the article in the pdf.
     '''
 
-    id_list, pdf = pdf_matching(pdf_path)
+    #Save uploaded_pdf
+    pdf = uploaded_pdf.file.read()
 
+    #Create temporary file/path
+    temp_pdf = NamedTemporaryFile(suffix='.pdf')
+    temp_pdf.write(pdf)
+
+    #Run functions
+    id_list, pdf = pdf_matching(temp_pdf.name)
     candidates_df = candidate_df(id_list)
-
     reviewers_df = select_reviewers(pdf, candidates_df)
+
+    #Close temporary file
+    temp_pdf.close()
 
     return reviewers_df
 
+@app.post("/upload_select")
+def create_upload_file(file: UploadFile):
+    pdf = file.file.read()
+    # with open('test.pdf', 'w') as file:
+    #     file.write(pdf)
+
+    temp_pdf = NamedTemporaryFile(suffix='.pdf')
+    #this will create a file with path:
+    #'curr_pdf\<filename>.pdf'
+    #Get filename with temp_pdf.name
+    temp_pdf.write(pdf)
+
+    return {"filename": temp_pdf.name}
 
 @app.get("/")
 def root():
---
+++ selection/frontend/app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import requests
+
+
+#PDF input
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+
+
+#URL
+url = 'http://localhost:8000/upload_select'
+
+files = {
+    'file': uploaded_file.getvalue()
+}
+
+###Can be deleted
+st.write(uploaded_file)
+
+
+#API request
+res = requests.post(url=url, files=files)
+
+
+###Output has to be changed
+print(res)
+if res:
+    st.write(res.json())
