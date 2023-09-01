0f7413c1f04297fb40cfa823ff8a08d5a1018772
Jannis Vajen <jvajen@gmail.com>
2023-09-01 12:27:26 +0200

    Add spinner to Streamlit frontend

--- selection/frontend/app.py
+++ selection/frontend/app.py
@@ -4,39 +4,33 @@ import pandas as pd
 import requests
 import streamlit as st
 
-# PDF input
-uploaded_file = st.file_uploader("Choose your .pdf file", type="pdf")
+
+def process_pdf(pdf_file):
+    res = requests.get(
+        url="http://localhost:8000/select",
+        files={"uploaded_pdf": pdf_file.getvalue()},
+    )
+
+    st.write()
+    potential_reviewers_dict = json.loads(res.json())
+    potential_reviewers_df = pd.DataFrame.from_dict(potential_reviewers_dict)
+
+    # Apply fixes to make st.dataframe() not choke on our dataframe object.
+    potential_reviewers_df["top_match"] = potential_reviewers_df["top_match"].apply(
+        lambda x: x[0]
+    )
+    potential_reviewers_df["second_match"] = potential_reviewers_df[
+        "second_match"
+    ].apply(lambda x: x[0])
+    potential_reviewers_df["third_match"] = potential_reviewers_df["third_match"].apply(
+        lambda x: x[0]
+    )
+
+    st.dataframe(potential_reviewers_df)
 
 
-# URL
-url = "http://localhost:8000/select"
-
-files = {"uploaded_pdf": uploaded_file.getvalue()}
-
-###Can be deleted
-st.write(uploaded_file)
-
-
-# API request
-res = requests.get(url=url, files=files)
-
-print("Done")
-print(res)
-print(type(res))
-###Output has to be changed
-# st.dataframe(res)
-potential_reviewers_dict = json.loads(res.json())
-potential_reviewers_df = pd.DataFrame.from_dict(potential_reviewers_dict)
-
-potential_reviewers_df["top_match"] = potential_reviewers_df["top_match"].apply(
-    lambda x: x[0]
-)
-potential_reviewers_df["second_match"] = potential_reviewers_df["second_match"].apply(
-    lambda x: x[0]
-)
-potential_reviewers_df["third_match"] = potential_reviewers_df["third_match"].apply(
-    lambda x: x[0]
-)
-
-
-st.dataframe(potential_reviewers_df)
+pdf_file = st.file_uploader("Choose your .pdf file", type="pdf")
+if pdf_file:
+    with st.spinner(text="Processing PDF"):
+        process_pdf(pdf_file)
+        st.success("Done")
