debccf929868978c910fc71f0d2039a5d92e864a
Tytus Wilam <tytus.wilam@gmail.com>
2023-09-13 11:37:26 +0200

    Change custom css

--- selection/frontend/app.py
+++ selection/frontend/app.py
@@ -4,13 +4,6 @@ import pandas as pd
 import requests
 import streamlit as st
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 BACKEND_URL = os.getenv(
     # "BACKEND_URL", "https://reviewerselectionv2-xybezttb3a-ew.a.run.app"
@@ -158,3 +151,17 @@ else:
             use_container_width=True,
         )
     )
+
+
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
