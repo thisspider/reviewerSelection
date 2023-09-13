552dd4635190bb1310ac0872d50fb35086c0b4d5
Tytus Wilam <tytus.wilam@gmail.com>
2023-09-13 10:29:37 +0200

    Add custom css to streamlit app

--- selection/frontend/app.py
+++ selection/frontend/app.py
@@ -150,3 +150,11 @@ else:
             use_container_width=True,
         )
     )
+
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
